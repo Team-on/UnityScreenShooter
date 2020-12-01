e0bb9b19e893dcbc4301e975c0d9720f17eefdd6
Team <timonsol13@gmail.com>
2020-12-01 22:07:45 +0200

    Change hierarchy to .git package

--- Runtime/Data/ScreenshooterSaveData.cs
+++ Runtime/Data/ScreenshooterSaveData.cs
@@ -4,17 +4,23 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.IO;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 [Serializable]
 public class ScreenshooterSaveData {
 	const string SAVE_FILE_NOREZ = "ScreenShooterSettings";
 	const string SAVE_FILE = "ScreenShooterSettings.json";
 	const string SAVE_FILE_EDITORPREFS = "ScreenShooterSettings.Save";
-	const string SAVE_FILE_EDITORPREFS_DEFAULT = "Plugins/ScreenShooter/Data/ScreenShooterSettings.json";
+	const string SAVE_FILE_EDITORPREFS_DEFAULT = "Editor/Setting/ScreenShooterSettings.json";
 
 	public string outputFolder = "-----";
-	public List<ScreenshotData> screenshoots = new List<ScreenshotData>(1) { new ScreenshotData() };
+	public List<ScreenshotData> screenshoots = new List<ScreenshotData>(1) {
+		new ScreenshotData("FullHD", new Vector2(1920, 1080)),
+		new ScreenshotData("FullHD", new Vector2(1920, 1080)){ captureOverlayUI = false},
+		new ScreenshotData("FullHD", new Vector2(1920, 1080)){ targetCamera = ScreenshooterTargetCamera.SceneView },
+	};
 
 	public static void SaveSettings(ScreenshooterSaveData data) {
 		if (!PlayerPrefs.HasKey(SAVE_FILE_EDITORPREFS)) {
@@ -40,7 +46,7 @@ public class ScreenshooterSaveData {
 	public static ScreenshooterSaveData LoadSettings() {
 		if (!PlayerPrefs.HasKey(SAVE_FILE_EDITORPREFS)) {
 #if UNITY_EDITOR
-			string[] allPath = AssetDatabase.FindAssets(SAVE_FILE_NOREZ);
+			string[] allPath = AssetDatabase.FindAssets(SAVE_FILE_NOREZ, new string[] { "Assets"});
 			if (allPath.Length != 0)
 				PlayerPrefs.SetString(SAVE_FILE_EDITORPREFS, AssetDatabase.GUIDToAssetPath(allPath[0]).Replace("Assets/", ""));
 #endif
